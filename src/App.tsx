3e785ea2987685e55725fe2a6a0d51a5bb540ba3
Damian Jamróz <mrlorem@gmail.com>
2023-07-03 11:57:25 +0200

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,6 @@ function App() {
 
 	const { data: images, isLoading } = useFetchImages({ imgPerPage, orderBy, currentPage });
 
-	console.log(images);
-
 	return (
 		<div className='main'>
 			<div className='navbar'>
